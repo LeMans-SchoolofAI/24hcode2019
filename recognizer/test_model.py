a2ab873703fab27894f45135a3f270d34c48178a
Antoine <antoine.mairesse@free.fr>
2019-01-19 20:08:23 +0100

    Build a userful class from initial script

--- recognizer/test_model.py
+++ recognizer/test_model.py
@@ -1,25 +1,25 @@
-from model import *
-
+from model import stop_sign_recognizer
+from torchvision import datasets, transforms
+import os
+import torch
 
 
 if __name__ == "__main__":
-    # Initialize the model for this run
-    model_ft, input_size = initialize_model(model_name, num_classes, feature_extract, use_pretrained=True)
-
+    model = stop_sign_recognizer()
+    
     # Print the model we just instantiated
-    print(f'Model created, input size is : {input_size}')
-    #print(model_ft) 
+    print(f'Model created, input size is : {model.input_size}')
 
     data_transforms = {
         'train': transforms.Compose([
-            transforms.RandomResizedCrop(input_size),
+            transforms.RandomResizedCrop(model.input_size),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
         'val': transforms.Compose([
-            transforms.Resize(input_size),
-            transforms.CenterCrop(input_size),
+            transforms.Resize(model.input_size),
+            transforms.CenterCrop(model.input_size),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
@@ -28,22 +28,16 @@ if __name__ == "__main__":
     print("Initializing Datasets and Dataloaders...")
 
     # Create training and validation datasets
-    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
+    image_datasets = {x: datasets.ImageFolder(os.path.join(model.data_dir, x), data_transforms[x]) for x in ['train', 'val']}
     # Create training and validation dataloaders
-    dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['train', 'val']}
+    dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=model.batch_size, shuffle=True, num_workers=4) for x in ['train', 'val']}
 
-    # Detect if we have a GPU available
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    # Send the model to GPU if possible
-    model_ft = model_ft.to(device)
-
-    # Set the model in eval mode
-    model_ft.eval()
+    # Set the model to evaluation mode
+    model.set_eval()
+    model.send_to_gpu()
 
     # Run the model on validation inputs
-    for inputs, labels in dataloaders_dict['val']:
-        outputs = model_ft(inputs)
+    for inputs, _ in dataloaders_dict['val']:
+        outputs = model.model(inputs)
         #print(f'inputs = {inputs}')
         print(f'outputs = {outputs}')
-        print(f'label = {labels}')
